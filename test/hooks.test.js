d10f9e735ab4804ae3969ad547e4d6d5916feb8d
mohamedisakr <mohamedisakr@gmail.com>
2020-09-20 18:43:27 +0200

    using the 4 types of hooks Mocha provides

---
+++ test/hooks.test.js
@@ -0,0 +1,21 @@
+describe("Mocha Hooks", () => {
+  before("Execute Before All Tests", () => {
+    console.log("Execute Before All Tests");
+  });
+
+  beforeEach("Execute Before Each Test", () => {
+    console.log("Execute Before Each Test");
+  });
+
+  after("Execute After All Tests", () => {
+    console.log("Execute After All Tests");
+  });
+
+  afterEach("Execute After Each Test", () => {
+    console.log("Execute Before Each Test");
+  });
+
+  it("Mocha Hooks Test", () => {
+    console.log("Mocha - This is a Test for Mocha Hooks");
+  });
+});
